1feec79491ef47c11e0eb9ac3c5dd89be441a986
nuyuyii <com5630248@gmail.com>
2016-09-12 02:38:00 +0700

    create xsl

--- MovieST/src/moviest/MovieST.java
+++ MovieST/src/moviest/MovieST.java
@@ -4,18 +4,56 @@
  * and open the template in the editor.
  */
 package moviest;
+// package dom;
+import javax.xml.parsers.DocumentBuilder; 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
-/**
- *
- * @author nuyuyii
- */
+// These classes are for the exceptions that can be thrown when the XML document is parsed:
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException; 
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.*;
+import org.xml.sax.SAXException;
+
+// These classes read the sample XML file and manage output:
+import java.io.*;
+
+
+// Finally, import the W3C definitions for a DOM, DOM exceptions, entities and nodes:
+
+import org.w3c.dom.Document;
+import org.w3c.dom.DocumentType;
+import org.w3c.dom.Entity;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+
 public class MovieST {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+        domFactory.setValidating(true);
+        DocumentBuilder builder = domFactory.newDocumentBuilder();
+        builder.setErrorHandler(new ErrorHandler() {
+            @Override
+            public void error(SAXParseException exception) throws SAXException {
+                // do something more useful in each of these handlers
+                exception.printStackTrace();
+            }
+            @Override
+            public void fatalError(SAXParseException exception) throws SAXException {
+                exception.printStackTrace();
+            }
+            @Override
+            public void warning(SAXParseException exception) throws SAXException {
+                exception.printStackTrace();
+            }
+        });
+        Document doc = builder.parse("/home/nuyuyii/NetBeansProjects/Pro_ST/MovieST/src/moviest/movies.xml");
     }
     
 }
